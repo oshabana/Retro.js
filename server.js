d5d8c8ae6ea8cc6e0b6f51602ef55ffd834b4077
oshabana <40475736+oshabana@users.noreply.github.com>
2020-09-06 19:09:26 -0400

    Cleaned up

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, "pub")));
-//console.log(path.join(__dirname, "public"));
-
-app.listen(port, () => {});
